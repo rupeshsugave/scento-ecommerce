c1b494ce53a34455b33b5b0c21194b5777218416
Rupesh Sugave <rupeshsugave@gmail.com>
2026-07-25 18:02:07 +0530

    Connect frontend with deployed backend

--- scento-frontend/src/services/api.ts
+++ scento-frontend/src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+
+  baseURL: "https://scento-backend-8q5o.onrender.com/api"
+
 });
 
 export default api;
