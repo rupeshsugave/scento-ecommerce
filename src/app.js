e5e00cd62f3350648dc3e4981098614bde880f3b
Rupesh Sugave <rupeshsugave@gmail.com>
2026-08-02 20:02:28 +0530

    Fixed image serving issue

--- src/app.js
+++ src/app.js
@@ -1,96 +1,52 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const userRoutes = require("./routes/userRoutes");
 const perfumeRoutes = require("./routes/perfumeRoutes");
 const cartRoutes = require("./routes/cartRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const addressRoutes = require("./routes/addressRoutes");
-
 const contactRoutes = require("./routes/contactRoutes");
 
 // Wishlist temporarily disabled
 // const wishlistRoutes = require("./routes/wishlistRoutes");
 
-
 const app = express();
-
 
 app.use(cors());
 
 app.use(express.json());
 
-
+// Serve static image files
+app.use(
+  "/images",
+  express.static(path.join(__dirname, "../images"))
+);
 
 app.get("/", (req, res) => {
-
   res.send("Scento Backend Running 🚀");
-
 });
 
-
-
 // User Routes
-
-app.use(
-  "/api/users",
-  userRoutes
-);
-
-
+app.use("/api/users", userRoutes);
 
 // Perfume Routes
-
-app.use(
-  "/api/perfumes",
-  perfumeRoutes
-);
-
-
+app.use("/api/perfumes", perfumeRoutes);
 
 // Cart Routes
-
-app.use(
-  "/api/cart",
-  cartRoutes
-);
-
-
+app.use("/api/cart", cartRoutes);
 
 // Order Routes
-
-app.use(
-  "/api/orders",
-  orderRoutes
-);
-
-
+app.use("/api/orders", orderRoutes);
 
 // Address Routes
+app.use("/api/addresses", addressRoutes);
 
-app.use(
-  "/api/addresses",
-  addressRoutes
-);
+// Contact Routes
+app.use("/api/contact", contactRoutes);
 
-
-
-// Contact Routes ✅
-
-app.use(
-  "/api/contact",
-  contactRoutes
-);
-
-
-
-// Wishlist Routes (temporary disabled)
-
-// app.use(
-//   "/api/wishlist",
-//   wishlistRoutes
-// );
-
-
+// Wishlist Routes (temporarily disabled)
+// app.use("/api/wishlist", wishlistRoutes);
 
 module.exports = app;
